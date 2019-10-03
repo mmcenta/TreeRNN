fc05caa35b5dc8b7123c28a364e047bc6c3bfdda
leonardocunha2107 <leonardocunha2107@gmail.com>
2019-10-03 20:37:41 +0200

    rcv1 loader begun

--- data/rcv1.py
+++ data/rcv1.py
@@ -1,3 +1,35 @@
 import chazutsu
+import json
+from os import path
+from tqdm import tqdm
 
-data=chazutsu.datasets.ReutersNews().download()
+def create_vocab_with_count(chazutsu_ressource,filename):
+    count={}
+    for t in tqdm(chazutsu_ressource.test_data()['words']):
+        t=t.split(' ')
+        for w in t:
+            if w not in count:
+                count[w]=1
+            else:
+                count[w]+=1
+    for t in tqdm(chazutsu_ressource.data()['words']):
+        t=t.split(' ')
+        for w in t:
+            if w not in count:
+                count[w]=1
+            else:
+                count[w]+=1
+
+    words=list(count.keys())
+    words.sort(key=lambda w: count[w],reverse=True)
+    
+    with open(filename,'w+') as fd:
+        json.dump(words,fd)
+class RCV1DS:
+    def __init__(self,vocab_size,filename=None):
+        if not filename:
+            filename=
+        
+if __name__=='__main__':           
+    data=chazutsu.datasets.ReutersNews().download()
+    create_vocab_with_count(data,'vocab.json')
